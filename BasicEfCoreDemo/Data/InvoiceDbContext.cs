fc2935bdce2650d713d5ba12c6e7dabd645f964b
prakash535kumar <prakash535kumar@gmail.com>
2024-05-25 00:24:48 +0530

    First Commit
    Created -
    1. WebAPI project
    2. Connection String
    3. AddDbContext
    4. Model
    5. Migration

---
+++ BasicEfCoreDemo/Data/InvoiceDbContext.cs
@@ -0,0 +1,11 @@
+﻿using BasicEfCoreDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicEfCoreDemo.Data
+{
+    public class InvoiceDbContext(DbContextOptions<InvoiceDbContext> options) : DbContext(options)
+    {
+        //public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Invoice> Invoices => Set<Invoice>();
+    }
+}
